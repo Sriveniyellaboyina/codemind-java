0d33fea08b8a998efa6cc9378015a1aa84b9f868
Sriveniyellaboyina <147792246+Sriveniyellaboyina@users.noreply.github.com>
2024-07-31 15:01:33 +0530

    import java.util.Scanner; public class Demo {     public static void  main(String[] args)     {         Scanner read=new Scanner(System.in);         int a,b,c;         a=read.nextInt();         b=read.nextInt();         c=a%b;         System.out.println(c);     }
    }

---
+++ Modulus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Demo
+{
+    public static void  main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int a,b,c;
+        a=read.nextInt();
+        b=read.nextInt();
+        c=a%b;
+        System.out.println(c);
+    }
+    
+}
